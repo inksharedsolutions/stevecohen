2034a7902e5a335fb00ee416905bf5e71c56f24d
inksharedsolutions <57968903+inksharedsolutions@users.noreply.github.com>
2021-04-08 23:51:31 +0800

    Update book-link.js

--- src/components/book-link/book-link.js
+++ src/components/book-link/book-link.js
@@ -12,7 +12,7 @@ const BookLink = (props)=>{
                     <nav className={`${props.data.isNonfront ? 'booklinkBlocks' : 'frontLinks'}`} >
                         <span>{`${props.data.isNonfront?'Ebook': ''}`}</span>
                         <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.amazon}>Amazon</a></li>
-                        <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.stratton}>Stratton Press</a></li>
+                        {/*<li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.stratton}>Stratton Press</a></li>*/}
                         <li><a rel="noopener noreferrer" target="_blank" href={props.data.ebooks.barnes}>Barnes & Noble</a></li>
                     </nav>
                 )} 
